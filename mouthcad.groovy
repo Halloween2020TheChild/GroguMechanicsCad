16fc9838ab21e3ed7180c4fa1730e67a00768c3b
Kevin Harrington <mad.hephaestus@gmail.com>
2022-08-18 21:29:35 -0400

    update cad engine

--- mouthcad.groovy
+++ mouthcad.groovy
@@ -1,1 +1,23 @@
+import com.neuronrobotics.bowlerstudio.creature.ICadGenerator
+import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics
+import com.neuronrobotics.sdk.addons.kinematics.MobileBase
+
+import eu.mihosoft.vrl.v3d.CSG
+
 //Your code here
+
+return new ICadGenerator() {
+
+	@Override
+	public ArrayList<CSG> generateCad(DHParameterKinematics arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return new ArrayList<CSG>() ;
+	}
+
+	@Override
+	public ArrayList<CSG> generateBody(MobileBase arg0) {
+		// TODO Auto-generated method stub
+		return new ArrayList<CSG>() ;
+	}
+	
+}
